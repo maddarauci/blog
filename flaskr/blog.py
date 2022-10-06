af9652e9c83ff701ffc994adb50a7687634edfc7
maddarauci <dominicd165@gmail.com>
2022-10-06 14:27:47 +1100

    modified:   flaskr/blog.py 	modified:   flaskr/templates/blog/index.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,12 +1,11 @@
 from flask import (
-	Blueprint, flash, g, redirect, render_template, request, url_for
+	Blueprint as bp, flash, g, redirect, render_template, request, url_for
 	)
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
@@ -18,5 +17,29 @@ def index():
 		).fetchall()
 	return render_template('blog/index.html', posts=posts)
 
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+	if request.method == 'POST':
+		title = request.form['title']
+		body = request.form['body']
+		error = None
 
+		if not title:
+			error = 'Title is required!.'
+		if error is not None:
+			flash(error)
+		else:
+			db = get_db()
+			db.execute(
+					'INSERT INTO post (title, body, author_id)'
+					' VALUES (?, ?, ?,)',
+					(title, body, g.user['id'])
+			)
+			db.commit()
+			return redirect(url_for('blod.index'))
+	return render_template('blog/create.html')
+
+
+bp = Blueprint('blog', __name__)
 print("Done")
