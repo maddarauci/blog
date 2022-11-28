f56da7d6da2c0d6fa8e53b69dfb6f983987b2148
maddarauci <dominicd165@gmail.com>
2022-11-28 23:13:27 +1100

    new file:   test/test_auth.py

---
+++ test/test_auth.py
@@ -0,0 +1,18 @@
+import pytest
+from flask import g, session
+from flaskr.db import get_db
+
+def test_register(client, app):
+	assert client.get('/auth/register').status_code == 200
+	response = client.post(
+		'auth/register', data={'username': 'a', 'password': 'a'}
+	)
+	assert response.header["Location"] == "/auth/login"
+
+	with app.app_context():
+		assert get_db().execute(
+			"SELECT * FROM user WHERE usename = 'a'",
+		).fetchone() is not None
+
+
+
